db7f30df162226f64666c78ac8fbcbdc3729441c
Brenden Picioane <brenden.picioane@gmail.com>
2020-09-05 14:34:00 -0400

    Finished 2.9

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,25 +13,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let bombMessage = "You Are Da Bomb!"
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = bombMessage
-            imageView.image = UIImage(named: "image2")
-        } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
-        }
+        print(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = (imageNumber + 1) % 10
+        
+        
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let bombMessage = "You Are Da Bomb!"
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = bombMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
+        
     }
     
 }
